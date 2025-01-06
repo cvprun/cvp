c94672be152192968859f25f17a6b3d9aee08782
zer0 <osom8979@gmail.com>
2025-01-06 09:18:15 +0900

    Fix error in FlowManager.add_node

--- cvp/flow/manager.py
+++ cvp/flow/manager.py
@@ -8,11 +8,8 @@ from type_serialize import deserialize, serialize
 from yaml import dump, full_load
 
 from cvp.flow.catalog import FlowCatalog
-from cvp.flow.datas.action import Action
 from cvp.flow.datas.graph import Graph
 from cvp.flow.datas.node import Node
-from cvp.flow.datas.pin import Pin
-from cvp.flow.datas.stream import Stream
 from cvp.flow.path import FlowPath
 from cvp.resources.home import HomeDir
 from cvp.strings.is_uuid import is_uuid4
@@ -94,48 +91,6 @@ class FlowManager(OrderedDict[str, Graph]):
 
     def add_node(self, graph: Graph, path: Union[str, FlowPath]) -> Node:
         node_template = self.get_node_template(path)
-        node_name = node_template.name
-        node_docs = node_template.docs
-        node_emblem = node_template.emblem
-        node_color = node_template.color
-
-        flow_inputs = list()
-        flow_outputs = list()
-
-        data_inputs = list()
-        data_outputs = list()
-
-        for pin_template in node_template.pins:
-            pin = Pin(
-                name=pin_template.name,
-                docs=pin_template.docs,
-                dtype=pin_template.dtype,
-                action=pin_template.action,
-                stream=pin_template.stream,
-                required=pin_template.required,
-            )
-
-            if pin.action == Action.flow and pin.stream == Stream.input:
-                flow_inputs.append(pin)
-            elif pin.action == Action.flow and pin.stream == Stream.output:
-                flow_outputs.append(pin)
-            elif pin.action == Action.data and pin.stream == Stream.input:
-                data_inputs.append(pin)
-            elif pin.action == Action.data and pin.stream == Stream.output:
-                data_outputs.append(pin)
-            else:
-                assert False, "Inaccessible section"
-
-        node = Node(
-            name=node_name,
-            docs=node_docs,
-            icon=node_emblem,
-            color=node_color,
-            flow_inputs=flow_inputs,
-            flow_outputs=flow_outputs,
-            data_inputs=data_inputs,
-            data_outputs=data_outputs,
-        )
-
+        node = Node.from_template(node_template)
         graph.nodes.append(node)
         return node
