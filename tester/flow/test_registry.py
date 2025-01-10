9e6f56c8a9e5e91dd295ff92d33c9b20d0f45ebc
zer0 <osom8979@gmail.com>
2025-01-10 16:41:17 +0900

    Create RegistryTestCase

---
+++ tester/flow/test_registry.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase, main
+
+from cvp.flow.registry import FlowRegistry, GlobalFlowRegistry, global_registry
+
+
+class RegistryTestCase(TestCase):
+    def test_global(self):
+        registry0 = global_registry()
+        registry1 = GlobalFlowRegistry()
+        registry2 = GlobalFlowRegistry()
+        self.assertEqual(registry0, registry1)
+        self.assertEqual(registry0, registry2)
+
+    def test_register_dtype(self):
+        registry = FlowRegistry()
+        self.assertEqual(0, len(registry.nodes))
+        self.assertEqual(0, len(registry.dtypes))
+
+
+if __name__ == "__main__":
+    main()
