d8e7ed109ab91a0f8c1ade33c8ff4a81adc6c2ef
zer0 <osom8979@gmail.com>
2025-02-20 19:48:01 +0900

    Remove @dataclass from FlowAnchor

--- cvp/flow/anchor.py
+++ cvp/flow/anchor.py
@@ -1,17 +1,72 @@
 # -*- coding: utf-8 -*-
 
-from dataclasses import dataclass
+from copy import copy, deepcopy
+from enum import StrEnum, auto, unique
+from typing import Any, Dict, Optional
 
+from type_serialize import Serializable
+
+from cvp.types.override import override
 from cvp.types.shapes import Point
 
 
-@dataclass
-class FlowAnchor:
-    x: float = 0.0
-    y: float = 0.0
+@unique
+class FlowAnchorKeys(StrEnum):
+    x = auto()
+    y = auto()
 
-    _selected: bool = False
-    _hovering: bool = False
+
+class FlowAnchor(Serializable):
+    Keys = FlowAnchorKeys
+
+    def __init__(
+        self,
+        x=0.0,
+        y=0.0,
+        *,
+        selected=False,
+        hovering=False,
+    ):
+        self.x = x
+        self.y = y
+        self._selected = selected
+        self._hovering = hovering
+
+    def __copy__(self):
+        cls = self.__class__
+        result = cls.__new__(cls)
+        result.x = copy(self.x)
+        result.y = copy(self.y)
+        result._selected = copy(self._selected)
+        result._hovering = copy(self._hovering)
+        return result
+
+    def __deepcopy__(self, memo: Optional[Dict[int, Any]] = None):
+        if memo is None:
+            memo = dict()
+        cls = self.__class__
+        result = cls.__new__(cls)
+        result.x = deepcopy(self.x, memo)
+        result.y = deepcopy(self.y, memo)
+        result._selected = deepcopy(self._selected, memo)
+        result._hovering = deepcopy(self._hovering, memo)
+        memo[id(self)] = result
+        return result
+
+    @override
+    def __serialize__(self) -> Any:
+        result = {self.Keys.x: self.x, self.Keys.y: self.y}
+        return {str(key): val for key, val in result.items()}
+
+    @override
+    def __deserialize__(self, data: Any) -> None:
+        if not isinstance(data, dict):
+            raise TypeError(f"Unexpected data type: {type(data).__name__}")
+
+        self.x = data.get(self.Keys.x, 0.0)
+        self.y = data.get(self.Keys.y, 0.0)
+        self._selected = False
+        self._hovering = False
 
     @property
     def point(self):
