eebaf4c8cfd8488945cfbbff268874b50ce2d0e8
zer0 <osom8979@gmail.com>
2025-03-05 10:30:51 +0900

    Trivial commit

--- cvp/pins/action.py
+++ cvp/pins/action.py
@@ -5,5 +5,12 @@ from enum import StrEnum, auto, unique
 
 @unique
 class Action(StrEnum):
-    data = auto()
     flow = auto()
+    """
+    Execution pins are used to connect nodes together to create a flow of execution.
+    """
+
+    data = auto()
+    """
+    Data pins are used for taking data into a node or outputting data from a node.
+    """
