b977042be876382b7939c2ae27799e84fba6ddc8
zer0 <osom8979@gmail.com>
2025-03-16 10:44:42 +0900

    Rename: *Ref.get -> *Ref.get_ref

--- cvp/dtypes/registry/ref.py
+++ cvp/dtypes/registry/ref.py
@@ -10,21 +10,18 @@ from cvp.dtypes.registry.registry import DtypeRegistry
 class DtypeRegistryRef:
     _ref: ReferenceType[DtypeRegistry]
 
-    def __init__(self, dtype_registry: Optional[DtypeRegistry] = None):
-        if dtype_registry is None:
-            dtype_registry = global_dtype_registry()
-        assert dtype_registry is not None
+    def __init__(self, dtype_registry: DtypeRegistry):
         self._ref = ref(dtype_registry)
-
-    def get(self) -> Optional[DtypeRegistry]:
-        return self._ref()
 
     @staticmethod
     def get_global() -> DtypeRegistry:
         return global_dtype_registry()
 
+    def get_ref(self) -> Optional[DtypeRegistry]:
+        return self._ref()
+
     def get_force(self) -> DtypeRegistry:
-        if registry := self.get():
+        if registry := self.get_ref():
             return registry
         else:
             return global_dtype_registry()
--- cvp/nodes/registry/ref.py
+++ cvp/nodes/registry/ref.py
@@ -10,21 +10,18 @@ from cvp.nodes.registry.registry import NodeRegistry
 class NodeRegistryRef:
     _ref: ReferenceType[NodeRegistry]
 
-    def __init__(self, node_registry: Optional[NodeRegistry] = None):
-        if node_registry is None:
-            node_registry = global_node_registry()
-        assert node_registry is not None
+    def __init__(self, node_registry: NodeRegistry):
         self._ref = ref(node_registry)
-
-    def get(self) -> Optional[NodeRegistry]:
-        return self._ref()
 
     @staticmethod
     def get_global() -> NodeRegistry:
         return global_node_registry()
 
+    def get_ref(self) -> Optional[NodeRegistry]:
+        return self._ref()
+
     def get_force(self) -> NodeRegistry:
-        if registry := self.get():
+        if registry := self.get_ref():
             return registry
         else:
             return global_node_registry()
