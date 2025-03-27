e13afe45c8144202d39ca2c54c254645392dd378
zer0 <osom8979@gmail.com>
2025-03-27 11:11:09 +0900

    Add dockspace test codes

--- cvp/apps/player/modes/preference.py
+++ cvp/apps/player/modes/preference.py
@@ -9,6 +9,8 @@ from cvp.config.sections.appearance import AppMode
 from cvp.imgui.dockspace import dockspace_context
 from cvp.msgs.msg import Msg
 from cvp.types.override import override
+from cvp.imgui.flags.dock_node import PASSTHRU_CENTRAL_NODE, AUTO_HIDE_TAB_BAR
+from cvp.imgui.flags.child import RESIZE_X, BORDERS
 
 
 class PreferenceMode(BaseMode):
@@ -35,11 +37,35 @@ class PreferenceMode(BaseMode):
 
     @override
     def do_process(self) -> None:
-        with dockspace_context("RootDockspace"):
-            imgui.begin("Left")
-            imgui.text("Content")
-            imgui.end()
+        # dockspace = imgui.dock_space_over_viewport(flags=PASSTHRU_CENTRAL_NODE | AUTO_HIDE_TAB_BAR)
+        # imgui.set_next_window_dock_id(dockspace)
 
-            imgui.begin("Right")
-            imgui.text("Content")
-            imgui.end()
+        # if (!ImGui::DockBuilderGetNode(dockspace_id)) {
+        # ImGui::DockBuilderRemoveNode(dockspace_id); // reset
+        # ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_DockSpace);
+        # ImGui::DockBuilderSetNodeSize(dockspace_id, ImGui::GetMainViewport()->Size);
+        #
+        # ImGuiID dock_main_id = dockspace_id;
+        # ImGuiID dock_left_id = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Left, 0.3f, nullptr, &dock_main_id);
+        # ImGuiID dock_right_id = dock_main_id;
+        #
+        # ImGui::DockBuilderDockWindow("Panel 1", dock_left_id);
+        # ImGui::DockBuilderDockWindow("Panel 2", dock_right_id);
+        # ImGui::DockBuilderFinish(dockspace_id);
+        # }
+
+        imgui.begin("Main")
+
+        height = -imgui.get_frame_height_with_spacing()
+
+        imgui.begin_child("Left", (150, height), child_flags=RESIZE_X | BORDERS)
+        imgui.text("Content")
+        imgui.end_child()
+
+        imgui.same_line()
+
+        imgui.begin_child("Right", (0, height))
+        imgui.text("Content")
+        imgui.end_child()
+
+        imgui.end()
--- cvp/imgui/dockspace.py
+++ cvp/imgui/dockspace.py
@@ -9,13 +9,19 @@ from cvp.imgui.flags import dock_node
 
 @contextmanager
 def dockspace_context(name: str):
-    viewport = imgui.get_main_viewport()
-    imgui.set_next_window_pos(viewport.work_pos)
-    imgui.set_next_window_size(viewport.work_size)
+    # viewport = imgui.get_main_viewport()
+    # imgui.set_next_window_pos(viewport.work_pos)
+    # imgui.set_next_window_size(viewport.work_size)
 
-    imgui.begin(name, None, dock_node.DOCKSPACE_FLAGS)
+    # imgui.begin(name, None, dock_node.DOCKSPACE_FLAGS)
+    # imgui.dock_space(dock_space_id, None, dock_node.DOCKSPACE_FLAGS)
+
     dock_space_id = imgui.get_id(name)
-    imgui.dock_space(dock_space_id, None, dock_node.DOCKSPACE_FLAGS)
+    imgui.dock_space_over_viewport(
+        dock_space_id,
+        imgui.get_main_viewport(),
+        dock_node.PASSTHRU_CENTRAL_NODE,
+    )
 
     try:
         yield dock_space_id
