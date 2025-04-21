ba20d4c24f4cf15635173068bca90a6b9df6c8fa
zer0 <osom8979@gmail.com>
2025-04-21 22:10:57 +0900

    Create _FlowLayout

--- cvp/apps/player/modes/flow/__init__.py
+++ cvp/apps/player/modes/flow/__init__.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from collections import OrderedDict
-from functools import lru_cache
-from typing import Sequence, Type
-
 from imgui_bundle import imgui
 from pygame.event import Event
 from pygame.key import ScancodeWrapper
 
 from cvp.apps.player.modes._base import BaseMode
-from cvp.apps.player.modes.flow._base import BaseFlowWindow, FlowWindowInterface
+from cvp.apps.player.modes.flow._layout import _FlowLayout
 from cvp.context.context import Context
 from cvp.imgui.dockspace import dockspace_over_viewport_context
 from cvp.imgui.flags.window import ROOT_STATIC_VIEWPORT_FLAGS
@@ -17,86 +13,13 @@ from cvp.msgs.msg import Msg
 from cvp.types.override import override
 
 
-@lru_cache
-def create_flow_window_types() -> Sequence[Type[BaseFlowWindow]]:
-    from cvp.apps.player.modes.flow.catalog import CatalogFlowWindow
-    from cvp.apps.player.modes.flow.debug import DebugFlowWindow
-    from cvp.apps.player.modes.flow.history import HistoryFlowWindow
-    from cvp.apps.player.modes.flow.logging import LoggingFlowWindow
-    from cvp.apps.player.modes.flow.props import PropsFlowWindow
-    from cvp.apps.player.modes.flow.tree import TreeFlowWindow
-
-    return (
-        CatalogFlowWindow,
-        DebugFlowWindow,
-        HistoryFlowWindow,
-        LoggingFlowWindow,
-        PropsFlowWindow,
-        TreeFlowWindow,
-    )
-
-
-def create_flow_window(context: Context) -> OrderedDict[str, FlowWindowInterface]:
-    win_types = create_flow_window_types()
-    return OrderedDict({wt.get_window_name(): wt(context) for wt in win_types})
-
-
-def initialize_dock_layout(dockspace_id: int) -> None:
-    from cvp.apps.player.modes.flow.catalog import CatalogFlowWindow
-    from cvp.apps.player.modes.flow.debug import DebugFlowWindow
-    from cvp.apps.player.modes.flow.history import HistoryFlowWindow
-    from cvp.apps.player.modes.flow.logging import LoggingFlowWindow
-    from cvp.apps.player.modes.flow.props import PropsFlowWindow
-    from cvp.apps.player.modes.flow.tree import TreeFlowWindow
-
-    imgui.internal.dock_builder_remove_node(dockspace_id)
-    imgui.internal.dock_builder_add_node(dockspace_id)
-
-    split = imgui.internal.dock_builder_split_node(dockspace_id, imgui.Dir.left, 0.1)
-    dock_left = split.id_at_dir
-    dock_main_right = split.id_at_opposite_dir
-
-    split = imgui.internal.dock_builder_split_node(dock_main_right, imgui.Dir.left, 0.8)
-    dock_main = split.id_at_dir
-    dock_right = split.id_at_opposite_dir
-
-    split = imgui.internal.dock_builder_split_node(dock_left, imgui.Dir.up, 0.6)
-    dock_left_top = split.id_at_dir
-    dock_left_bottom = split.id_at_opposite_dir
-
-    split = imgui.internal.dock_builder_split_node(dock_main, imgui.Dir.down, 0.1)
-    dock_main_bottom = split.id_at_dir
-    # dock_main_top = split.id_at_opposite_dir
-
-    split = imgui.internal.dock_builder_split_node(dock_right, imgui.Dir.up, 0.6)
-    dock_right_top = split.id_at_dir
-    dock_right_bottom = split.id_at_opposite_dir
-
-    catalog_name = CatalogFlowWindow.get_window_name()
-    debug_name = DebugFlowWindow.get_window_name()
-    history_name = HistoryFlowWindow.get_window_name()
-    logging_name = LoggingFlowWindow.get_window_name()
-    props_name = PropsFlowWindow.get_window_name()
-    tree_name = TreeFlowWindow.get_window_name()
-
-    imgui.internal.dock_builder_dock_window(tree_name, dock_left_top)
-    imgui.internal.dock_builder_dock_window(catalog_name, dock_left_bottom)
-
-    imgui.internal.dock_builder_dock_window(logging_name, dock_main_bottom)
-    imgui.internal.dock_builder_dock_window(debug_name, dock_main_bottom)
-
-    imgui.internal.dock_builder_dock_window(props_name, dock_right_top)
-    imgui.internal.dock_builder_dock_window(history_name, dock_right_bottom)
-
-    imgui.internal.dock_builder_finish(dockspace_id)
-
-
 class FlowMode(BaseMode):
     __cvp_mode_name__ = "Flow"
 
     def __init__(self, context: Context):
         super().__init__(context)
-        self._windows = create_flow_window(context)
+        self._layout = _FlowLayout()
+        self._windows = self._layout.create_windows(context)
         self._viewport_flags = ROOT_STATIC_VIEWPORT_FLAGS
         self._initialized_dock_layout = False
 
@@ -118,12 +41,12 @@ class FlowMode(BaseMode):
 
     @override
     def do_process(self) -> None:
-        with dockspace_over_viewport_context() as dockspace_id:
+        viewport = imgui.get_main_viewport()
+        with dockspace_over_viewport_context(viewport=viewport) as dockspace_id:
             assert isinstance(dockspace_id, int)
             assert 0 <= dockspace_id
-            if not self._initialized_dock_layout:
-                initialize_dock_layout(dockspace_id)
-                self._initialized_dock_layout = True
+            if not self._layout.initialized:
+                self._layout.initialize_dock_layout(dockspace_id, viewport)
 
         for window in self._windows.values():
             window.do_process()
---
+++ cvp/apps/player/modes/flow/_layout.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+
+from collections import OrderedDict
+from typing import Final, Sequence, Type
+
+from imgui_bundle import imgui
+
+from cvp.apps.player.modes.flow._base import BaseFlowWindow, FlowWindowInterface
+from cvp.context.context import Context
+
+DOCK_SPACE_FLAG: Final[int] = int(imgui.internal.DockNodeFlagsPrivate_.dock_space.value)
+DOCKING_ENABLE_FLAG: Final[int] = int(imgui.ConfigFlags_.docking_enable.value)
+
+
+def split_node(node_id: int, split_dir: imgui.Dir, ratio: float):
+    return imgui.internal.dock_builder_split_node(node_id, split_dir, ratio)
+
+
+def dock_window(window_name: str, node_id: int) -> None:
+    imgui.internal.dock_builder_dock_window(window_name, node_id)
+
+
+class _FlowLayout:
+    types: Sequence[Type[BaseFlowWindow]]
+
+    def __init__(self):
+        from cvp.apps.player.modes.flow.catalog import CatalogFlowWindow
+        from cvp.apps.player.modes.flow.debug import DebugFlowWindow
+        from cvp.apps.player.modes.flow.history import HistoryFlowWindow
+        from cvp.apps.player.modes.flow.logging import LoggingFlowWindow
+        from cvp.apps.player.modes.flow.props import PropsFlowWindow
+        from cvp.apps.player.modes.flow.tree import TreeFlowWindow
+
+        self._initialized_dock_layout = False
+
+        self.catalog = CatalogFlowWindow.get_window_name()
+        self.debug = DebugFlowWindow.get_window_name()
+        self.history = HistoryFlowWindow.get_window_name()
+        self.logging = LoggingFlowWindow.get_window_name()
+        self.props = PropsFlowWindow.get_window_name()
+        self.tree = TreeFlowWindow.get_window_name()
+
+        self.types = (
+            CatalogFlowWindow,
+            DebugFlowWindow,
+            HistoryFlowWindow,
+            LoggingFlowWindow,
+            PropsFlowWindow,
+            TreeFlowWindow,
+        )
+
+    @property
+    def initialized(self) -> bool:
+        return self._initialized_dock_layout
+
+    def create_windows(self, context: Context) -> OrderedDict[str, FlowWindowInterface]:
+        return OrderedDict({wt.get_window_name(): wt(context) for wt in self.types})
+
+    def _initialize_dock_layout(
+        self,
+        dockspace_id: int,
+        viewport: imgui.Viewport,
+        left_ratio=0.15,
+        right_ratio=0.15,
+        left_up_ratio=0.60,
+        right_up_ratio=0.60,
+        bottom_ratio=0.25,
+    ) -> None:
+        imgui.internal.dock_builder_add_node(dockspace_id, DOCK_SPACE_FLAG)
+        imgui.internal.dock_builder_set_node_size(dockspace_id, viewport.work_size)
+
+        split_result = split_node(dockspace_id, imgui.Dir.left, left_ratio)
+        dock_left = split_result.id_at_dir
+        dock_main_right = split_result.id_at_opposite_dir
+
+        split_result = split_node(dock_main_right, imgui.Dir.right, right_ratio)
+        dock_right = split_result.id_at_dir
+        dock_center = split_result.id_at_opposite_dir
+
+        split_result = split_node(dock_left, imgui.Dir.up, left_up_ratio)
+        dock_left_top = split_result.id_at_dir
+        dock_left_bottom = split_result.id_at_opposite_dir
+
+        split_result = split_node(dock_right, imgui.Dir.up, right_up_ratio)
+        dock_right_top = split_result.id_at_dir
+        dock_right_bottom = split_result.id_at_opposite_dir
+
+        split_result = split_node(dock_center, imgui.Dir.down, bottom_ratio)
+        dock_center_bottom = split_result.id_at_dir
+        # dock_center_top = split_result.id_at_opposite_dir
+
+        dock_window(self.tree, dock_left_top)
+        dock_window(self.catalog, dock_left_bottom)
+
+        dock_window(self.props, dock_right_top)
+        dock_window(self.history, dock_right_bottom)
+
+        dock_window(self.logging, dock_center_bottom)
+        dock_window(self.debug, dock_center_bottom)
+
+        # dock_left_top_node = imgui.internal.dock_builder_get_node(dock_left_top)
+        # dock_left_top_node.local_flags |= imgui.DockNodeFlags_.no_docking_split
+        # dock_left_top_node.local_flags |= imgui.DockNodeFlags_.no_resize
+        # dock_left_top_node.local_flags |= imgui.DockNodeFlags_.no_undocking
+
+    @staticmethod
+    def enabled_docking() -> bool:
+        return bool(imgui.get_io().config_flags & DOCKING_ENABLE_FLAG)
+
+    def initialize_dock_layout(
+        self,
+        dockspace_id: int,
+        viewport: imgui.Viewport,
+    ) -> None:
+        if not self.enabled_docking():
+            return
+
+        if self._initialized_dock_layout:
+            return
+
+        imgui.internal.dock_builder_remove_node(dockspace_id)
+        try:
+            self._initialize_dock_layout(dockspace_id, viewport)
+        finally:
+            imgui.internal.dock_builder_finish(dockspace_id)
+            self._initialized_dock_layout = True
