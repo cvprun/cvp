eb4f627b13d185b795e73ebaf6f971a7d983fd1a
zer0 <osom8979@gmail.com>
2025-04-22 15:53:58 +0900

    Create Trigonometry

---
+++ cvp/maths/trigonometry/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# https://en.wikipedia.org/wiki/Trigonometry
+#
+#                  B
+#                  *
+#                 /|
+#                / |
+# (hypotenuse)  /  |
+#          h   /   |   a
+#             /    |  (opposite)
+#            /     |
+#        A  *------*  C (right angle, 90-degree)
+#              b
+#         (adjacent)
+#
+# sin(A) = a/h
+# cos(A) = b/h
+# tan(A) = a/b
+#
+# cosecant (csc), csc(A) = 1/sin(A) = h/a
+# secant (sec), sec(A) = 1/cos(A) = h/b
+# cotangent (cot), cot(A) = 1/tan(A) = b/a
---
+++ cvp/maths/trigonometry/functions/cosine.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# https://en.wikipedia.org/wiki/Law_of_cosines
