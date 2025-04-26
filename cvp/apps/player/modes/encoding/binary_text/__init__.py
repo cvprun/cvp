4fd7f456bdef819b2672a8bbb242b94e4470ae52
zer0 <osom8979@gmail.com>
2025-04-26 10:21:37 +0900

    Implement BinaryTextMode

--- cvp/apps/player/modes/encoding/binary_text/__init__.py
+++ cvp/apps/player/modes/encoding/binary_text/__init__.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from typing import Final, Optional
+from enum import Enum, auto, unique
+from typing import Final, Optional, Union
 
 from imgui_bundle import imgui
 
 from cvp.apps.player.modes._base import BaseMode
 from cvp.context.context import Context
-from cvp.encoding.binary_text import BinaryToTextEncodingMethod
+from cvp.encoding.binary_text import (
+    BinaryToText,
+    binary_to_text_decoding,
+    binary_to_text_encoding,
+)
 from cvp.imgui.begin_child import begin_child_context
 from cvp.imgui.fit_size import FIT_SIZE
 from cvp.imgui.flags.child import BORDERS, RESIZE_X
+from cvp.imgui.input_text_multilingual import input_text_multiline
 from cvp.imgui.text_centered import text_centered
 from cvp.types.override import override
 
@@ -20,21 +26,84 @@ class BinaryTextMode(BaseMode):
     _MENU_SPLIT_X: Final[int] = 300
     _MENU_CHILD_FLAGS: Final[int] = RESIZE_X | BORDERS
 
+    @unique
+    class _TranscodeDirection(Enum):
+        encoding = auto()
+        decoding = auto()
+
     def __init__(self, context: Context):
         super().__init__(context)
-        self._methods = [str(m) for m in BinaryToTextEncodingMethod]
+        self._methods = [str(m) for m in BinaryToText]
+        self._input = str()
+        self._output = str()
+        self._input_error: Optional[Union[BaseException, str]] = None
+        self._output_error: Optional[Union[BaseException, str]] = None
+        self._encoding = "utf-8"
+        self._errors = "strict"
+        self._last_transcoding = self._TranscodeDirection.encoding
+
+    @property
+    def error_color(self):
+        return self.context.config.appearance.error_color
 
     @override
     def do_process(self) -> None:
         with self.begin_mode_context():
             self.do_child_process()
 
-    def get_selected_method(self) -> Optional[BinaryToTextEncodingMethod]:
+    def get_method(self, method_name: Optional[str] = None) -> Optional[BinaryToText]:
+        if not method_name:
+            method_name = self.selected
+        assert isinstance(method_name, str)
+
         try:
-            index = self._methods.index(self.selected)
-            return BinaryToTextEncodingMethod(self._methods[index])
+            index = self._methods.index(method_name)
+            return BinaryToText(self._methods[index])
         except ValueError:
             return None
+
+    def update_encode(self, method: BinaryToText, value: str) -> None:
+        self._input = value
+        self._input_error = None
+
+        try:
+            data = self._input.encode(encoding=self._encoding, errors=self._errors)
+            self._output = binary_to_text_encoding(method, data)
+            self._output_error = None
+        except BaseException as e:
+            self._output = str()
+            self._output_error = e if str(e) else type(e).__name__
+        finally:
+            self._last_transcoding = self._TranscodeDirection.encoding
+
+    def update_decode(self, method: BinaryToText, value: str) -> None:
+        self._output = value
+        self._output_error = None
+
+        try:
+            data = binary_to_text_decoding(method, self._output)
+            self._input = str(data, encoding=self._encoding, errors=self._errors)
+            self._input_error = None
+        except BaseException as e:
+            self._input = str()
+            self._input_error = e if str(e) else type(e).__name__
+        finally:
+            self._last_transcoding = self._TranscodeDirection.decoding
+
+    def update_with_last_transcoding(self, method: BinaryToText) -> None:
+        match self._last_transcoding:
+            case self._TranscodeDirection.encoding:
+                self.update_encode(method, self._input)
+            case self._TranscodeDirection.decoding:
+                self.update_encode(method, self._output)
+
+    def clear_fields(self) -> None:
+        self._input = str()
+        self._output = str()
+
+    @staticmethod
+    def convert_displayable_text(method_name: str) -> str:
+        return method_name.replace("_", "-").capitalize()
 
     def do_child_process(self) -> None:
         with begin_child_context(
@@ -45,8 +114,14 @@ class BinaryTextMode(BaseMode):
             if imgui.begin_list_box("##List", FIT_SIZE):
                 try:
                     for method_name in self._methods:
+                        display = self.convert_displayable_text(method_name)
+                        label = f"{display}##{method_name}"
                         selected = method_name == self.selected
-                        if imgui.selectable(method_name, selected)[1]:
+                        if imgui.selectable(label, selected)[1]:
+                            if self.selected != method_name:
+                                selected_method = self.get_method(method_name)
+                                assert selected_method is not None
+                                self.update_with_last_transcoding(selected_method)
                             self.selected = method_name
                 finally:
                     imgui.end_list_box()
@@ -54,10 +129,54 @@ class BinaryTextMode(BaseMode):
         imgui.same_line()
 
         with begin_child_context("Main"):
-            if method := self.get_selected_method():
+            if method := self.get_method():
                 self.do_method_process(method)
             else:
                 text_centered("Please select a item")
 
-    def do_method_process(self, method: BinaryToTextEncodingMethod) -> None:
-        pass
+    def do_method_process(self, method: BinaryToText) -> None:
+        display = self.convert_displayable_text(method)
+        imgui.text(f"Binary To Text : {display}")
+        imgui.same_line()
+        if imgui.small_button("Clear"):
+            self.clear_fields()
+        imgui.separator()
+
+        available_space = imgui.get_content_region_avail()
+        item_spacing = imgui.get_style().item_spacing
+        total_empty_width = item_spacing.x * 2.0  # center and right padding
+        half_width = (available_space.x - total_empty_width) / 2.0
+
+        with begin_child_context("MainLeft", (half_width, 0), RESIZE_X):
+            imgui.text("Input")
+
+            if imgui.button("Clipboard"):
+                self.update_encode(method, imgui.get_clipboard_text())
+            imgui.same_line()
+            if imgui.button("Copy"):
+                imgui.set_clipboard_text(self._input)
+
+            if self._input_error is not None:
+                imgui.text_colored(self.error_color, str(self._input_error))
+            else:
+                input_result = input_text_multiline("##Input", self._input, FIT_SIZE)
+                if self._input != input_result.value:
+                    self.update_encode(method, input_result.value)
+
+        imgui.same_line()
+
+        with begin_child_context("MainBottom"):
+            imgui.text("Output")
+
+            if imgui.button("Clipboard"):
+                self.update_decode(method, imgui.get_clipboard_text())
+            imgui.same_line()
+            if imgui.button("Copy"):
+                imgui.set_clipboard_text(self._output)
+
+            if self._output_error is not None:
+                imgui.text_colored(self.error_color, str(self._output_error))
+            else:
+                output_result = input_text_multiline("##Output", self._output, FIT_SIZE)
+                if self._output != output_result.value:
+                    self.update_decode(method, output_result.value)
--- cvp/encoding/binary_text.py
+++ cvp/encoding/binary_text.py
@@ -1,12 +1,89 @@
 # -*- coding: utf-8 -*-
 # https://en.wikipedia.org/wiki/Binary-to-text_encoding
 
+import base64
+import quopri
+import sys
+import urllib.parse
 from enum import StrEnum, auto, unique
+from functools import partial
+from typing import Callable, Dict, Final, Union
 
 
 @unique
-class BinaryToTextEncodingMethod(StrEnum):
+class BinaryToText(StrEnum):
+    base85 = auto()
     base64 = auto()  # https://en.wikipedia.org/wiki/Base64
+    base32 = auto()
+    base16 = auto()
+    ascii85 = auto()
+    z85 = auto()
     mime = auto()  # https://en.wikipedia.org/wiki/MIME
     percent = auto()  # https://en.wikipedia.org/wiki/Percent-encoding
-    quoted = auto()  # https://en.wikipedia.org/wiki/Quoted-printable
+    quoted_printable = auto()  # https://en.wikipedia.org/wiki/Quoted-printable
+
+
+# fmt:off
+if (3, 13) <= sys.version_info:
+    _z85encode = base64.z85encode  # noqa
+    _z85decode = base64.z85decode  # noqa
+else:
+    def _z85encode(_):
+        raise NotImplementedError("Added in python version 3.13")
+
+    def _z85decode(_):
+        raise NotImplementedError("Added in python version 3.13")
+# fmt:on
+
+
+def _mime_encode(_):
+    raise NotImplementedError
+
+
+def _mime_decode(_):
+    raise NotImplementedError
+
+
+def _percent_encode(bs: Union[bytes, bytearray]) -> bytes:
+    return urllib.parse.quote_from_bytes(bs).encode(encoding="utf-8")
+
+
+def _percent_decode(bs: Union[str, bytes, bytearray]) -> bytes:
+    return urllib.parse.unquote_to_bytes(bs)
+
+
+_BINARY_TO_TEXT_ENCODING_FUNCS: Final[Dict[BinaryToText, Callable]] = {
+    BinaryToText.base85: partial(base64.b85encode, pad=False),
+    BinaryToText.base64: partial(base64.b64encode, altchars=None),
+    BinaryToText.base32: base64.b32encode,
+    BinaryToText.base16: base64.b16encode,
+    BinaryToText.ascii85: base64.a85encode,
+    BinaryToText.z85: _z85encode,
+    BinaryToText.mime: _mime_encode,
+    BinaryToText.percent: _percent_encode,
+    BinaryToText.quoted_printable: partial(quopri.encodestring, header=False),
+}
+
+_BINARY_TO_TEXT_DECODING_FUNCS: Final[Dict[BinaryToText, Callable]] = {
+    BinaryToText.base85: base64.b85decode,
+    BinaryToText.base64: partial(base64.b64decode, altchars=None, validate=False),
+    BinaryToText.base32: base64.b32encode,
+    BinaryToText.base16: base64.b16encode,
+    BinaryToText.ascii85: base64.a85decode,
+    BinaryToText.z85: _z85decode,
+    BinaryToText.mime: _mime_decode,
+    BinaryToText.percent: _percent_decode,
+    BinaryToText.quoted_printable: partial(quopri.decodestring, header=False),
+}
+
+
+def binary_to_text_encoding(method: BinaryToText, data: bytes, encoding="utf-8") -> str:
+    result = _BINARY_TO_TEXT_ENCODING_FUNCS[method](data)
+    assert isinstance(result, bytes)
+    return str(result, encoding=encoding)
+
+
+def binary_to_text_decoding(method: BinaryToText, data: str) -> bytes:
+    result = _BINARY_TO_TEXT_DECODING_FUNCS[method](data)
+    assert isinstance(result, bytes)
+    return result
