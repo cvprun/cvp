245af95c4abad86cb5ccd5c8fda3bacf1b65db58
zer0 <osom8979@gmail.com>
2025-05-27 16:58:40 +0900

    Add mode: ModelingMode

---
+++ cvp/apps/player/modes/modeling/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from cvp.apps.player.modes._base import BaseMode
+from cvp.assets.fonts.mdi import CUBE
+from cvp.context.context import Context
+from cvp.imgui.begin_child import begin_child_context
+from cvp.types.override import override
+
+
+class ModelingMode(BaseMode):
+    __cvp_mode_name__ = "Modeling"
+    __cvp_mode_icon__ = CUBE
+
+    def __init__(self, context: Context):
+        super().__init__(context)
+
+    @override
+    def on_process(self) -> None:
+        with self.begin_mode_context():
+            with begin_child_context("Main"):
+                pass
