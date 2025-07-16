ae414c34749a13c4c4a4f0dde0fc5abcb46f11c6
zer0 <osom8979@gmail.com>
2025-07-16 10:29:50 +0900

    Create UTC_OFFSETS

--- cvp/chrono/timezones.py
+++ cvp/chrono/timezones.py
@@ -1,21 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
+from types import MappingProxyType
 from typing import Final
 from zoneinfo import ZoneInfo, available_timezones
 
 from cvp.containers.immutable_list import ImmutableList
-
-
-def _create_sorted_available_timezones() -> ImmutableList[ZoneInfo]:
-    names = list(available_timezones())
-    names.sort()
-    zones = [ZoneInfo(name) for name in names]
-    return ImmutableList(zones)
-
-
-TIMEZONES: Final[ImmutableList[ZoneInfo]] = _create_sorted_available_timezones()
-ZONE_NAMES: Final[ImmutableList[str]] = ImmutableList(map(lambda x: x.key, TIMEZONES))
 
 
 def format_utc_offset(tz: ZoneInfo) -> str:
@@ -28,3 +18,23 @@ def format_utc_offset(tz: ZoneInfo) -> str:
 
     sign = "+" if total_seconds >= 0 else "-"
     return f"{sign}{abs(hours):02d}:{minutes:02d}"
+
+
+def _create_sorted_available_timezones() -> ImmutableList[ZoneInfo]:
+    names = list(available_timezones())
+    names.sort()
+    zones = [ZoneInfo(name) for name in names]
+    return ImmutableList(zones)
+
+
+def _create_zone_names() -> ImmutableList[str]:
+    return ImmutableList(map(lambda x: x.key, TIME_ZONES))
+
+
+def _create_utc_offsets() -> MappingProxyType[str, str]:
+    return MappingProxyType({tz.key: format_utc_offset(tz) for tz in TIME_ZONES})
+
+
+TIME_ZONES: Final[ImmutableList[ZoneInfo]] = _create_sorted_available_timezones()
+ZONE_NAMES: Final[ImmutableList[str]] = _create_zone_names()
+UTC_OFFSETS: Final[MappingProxyType[str, str]] = _create_utc_offsets()
--- tester/chrono/test_timezones.py
+++ tester/chrono/test_timezones.py
@@ -3,12 +3,13 @@
 from unittest import TestCase, main
 from zoneinfo import ZoneInfo
 
-from cvp.chrono.timezones import format_utc_offset
+from cvp.chrono.timezones import UTC_OFFSETS, format_utc_offset
 
 
 class TimezonesTestCase(TestCase):
     def test_format_utc_offset(self):
         self.assertEqual("+09:00", format_utc_offset(ZoneInfo("Asia/Seoul")))
+        self.assertEqual("+09:00", UTC_OFFSETS["Asia/Seoul"])
 
 
 if __name__ == "__main__":
