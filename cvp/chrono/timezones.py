dc3403581c64bf1349879c289256bcef60f92954
zer0 <osom8979@gmail.com>
2025-07-16 10:18:06 +0900

    Create format_utc_offset

--- cvp/chrono/timezones.py
+++ cvp/chrono/timezones.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 from typing import Final
 from zoneinfo import ZoneInfo, available_timezones
 
@@ -15,3 +16,15 @@ def _create_sorted_available_timezones() -> ImmutableList[ZoneInfo]:
 
 TIMEZONES: Final[ImmutableList[ZoneInfo]] = _create_sorted_available_timezones()
 ZONE_NAMES: Final[ImmutableList[str]] = ImmutableList(map(lambda x: x.key, TIMEZONES))
+
+
+def format_utc_offset(tz: ZoneInfo) -> str:
+    offset = datetime.now(tz).utcoffset()
+    assert offset is not None
+
+    total_seconds = offset.total_seconds()
+    hours, remainder = divmod(int(total_seconds), 3600)
+    minutes = remainder // 60
+
+    sign = "+" if total_seconds >= 0 else "-"
+    return f"{sign}{abs(hours):02d}:{minutes:02d}"
---
+++ tester/chrono/test_timezones.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase, main
+from zoneinfo import ZoneInfo
+
+from cvp.chrono.timezones import format_utc_offset
+
+
+class TimezonesTestCase(TestCase):
+    def test_format_utc_offset(self):
+        self.assertEqual("+09:00", format_utc_offset(ZoneInfo("Asia/Seoul")))
+
+
+if __name__ == "__main__":
+    main()
